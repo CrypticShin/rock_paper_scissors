5f0eac8905292219171091b89e13c13f68c4420b
Shin <crypticshin@gmail.com>
2023-06-13 18:23:33 +0100

    Create playRound(playerSelection, computerSelection)
    playRound() performs the game logic
    it compares playerSelection with computerSelection and returns the winner based on the games rules

--- index.js
+++ index.js
@@ -3,7 +3,49 @@ let choices = ["rock", "paper", "scissors"];
 
 function getCOmputerChoice() {
   let computerChoice = choices[Math.floor(Math.random() * choices.length)];
-  console.log(computerChoice);
+  return computerChoice;
 }
 
-getCOmputerChoice();
+/* 
+user rock, computer scissors => user wins
+          , computer rock => tie
+          , computer paper => computer wins
+user scissors, computer rock => Computer wins
+              , computer scissors => tie
+              , computer paper => User wins
+user paper, computer scissors => computer wins
+          , computer paper => tie
+          , computer rock => user wins
+*/ 
+
+function playRound(playerSelection, computerSelection) {
+  playerSelection.toLowerCase();
+  if(playerSelection == "rock") {
+    if(computerSelection == "scissors") {
+      return "You WIN! Rock beats Scissors.";
+    } else if(computerSelection == "rock") {
+      return "It's a TIE! Wanna go again?";
+    } else {
+      return "You LOSE! Paper beats Rock.";
+    }
+  } else if(playerSelection == "scissors") {
+    if(computerSelection == "rock") {
+      return "You LOSE! Rock beats Scissors.";
+    } else if(computerSelection == "scissors") {
+      return "It's a TIE! Wanna go again?"
+    } else {
+      return "Yu WIN! Scissors beats Paper."
+    }
+  } else if (playerSelection == "paper") {
+    if(computerSelection == "scissors") {
+      return "You LOSE! Scissors beats Paper.";
+    } else if(computerSelection == "paper") {
+      return "It's a TIE! Wanna go again?"
+    } else {
+      return "You WIN! Paper beats Rock.";
+    }
+  } else {
+    return "Enter a valid option!"
+  }
+}
+
